522139e6b71119ebfefb0c52c58e9906e2feb9f8
Lee-Roy King <leeroywking@gmail.com>
2019-09-07 14:33:13 -0700

    merp

---
+++ src/components/app.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { connect } from "react-redux";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import "./app.scss";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  handleButtonClickUp = e => {
+    e.preventDefault();
+    this.setState({ counter: ++this.state.counter });
+  };
+
+  handleButtonClickDown = e => {
+    e.preventDefault();
+    this.setState({ counter: --this.state.counter });
+  };
+
+  render() {
+    return (
+      <div id="main">
+        <button
+          class="clicker"
+          id="clickdown"
+          onClick={this.handleButtonClickDown}
+        >
+          -1
+        </button>
+        <h4 id="counter">{this.state.counter}</h4>
+        <button class="clicker" id="clickup" onClick={this.handleButtonClickUp}>
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Counter />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
